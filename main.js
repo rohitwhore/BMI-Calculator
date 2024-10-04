dcaba84ebf6e6189c7c5cc43d1bbe042afb8d16a
Rohit Roy <133909963+rohitwhore@users.noreply.github.com>
2024-10-04 22:20:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form')
+
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+
+   const height = parseInt(document.querySelector('#height').value);
+   const weight = parseInt(document.querySelector('#weight').value);
+   const result = document.querySelector('#results');
+
+    if(height === '' || height < 0 || isNaN(height)){
+        result.innerHTML = `please give a valid height ${height}`
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
+        result.innerHTML = `please give a valid weight ${weight}`
+    }else{
+        const bmi = (weight /((height*height)/10000)).toFixed(2)
+        if(bmi < 18.6){
+            msg.innerHTML = 'Under Weight'
+        }else if(bmi > 18.6 && bmi < 24.9){
+            msg.innerHTML = 'Normal'
+        }else{
+            msg.innerHTML = 'Overwight'
+        }
+        result.innerHTML = `<span>${bmi}</span>`
+    }
+}); 
